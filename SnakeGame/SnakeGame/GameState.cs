0384ffe29c343f5e0e6374cc9f373fb78e380b9e
Mohamad6969 <mohammadsharkawi17@gmail.com>
2023-03-24 18:57:57 +0100

    game finished....maybe..idk

--- SnakeGame/SnakeGame/GameState.cs
+++ SnakeGame/SnakeGame/GameState.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace SnakeGame
 {
@@ -15,6 +16,8 @@ namespace SnakeGame
         public int Score { get; private set; }  
         public bool GameOver { get; private set; }
 
+
+        private readonly LinkedList<Direction> dirChanges = new LinkedList<Direction>();
         private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
         private readonly Random random = new Random();
 
@@ -100,9 +103,33 @@ namespace SnakeGame
             snakePositions.RemoveLast();
         }
 
+        private Direction GetLastDirection()
+        {
+            if(dirChanges.Count == 0)
+            {
+                return Dir;
+            }
+            return dirChanges.Last.Value;
+        }
+        private bool CanChangeDirection(Direction newDir)
+        {
+            if(dirChanges.Count == 2) 
+            {
+                return false;
+            }
+
+            Direction lastDir = GetLastDirection();
+            return newDir != lastDir && newDir != lastDir.Opposite();
+        }
+
         public void ChangeDirection(Direction dir)
         {
-            Dir = dir;
+            if (CanChangeDirection(dir))
+            {
+                dirChanges.AddLast(dir);
+            }
+
+            
         }
 
         private bool OutsideGrid(Position pos)
@@ -128,6 +155,12 @@ namespace SnakeGame
 
         public void Move()
         {
+            if(dirChanges.Count > 0) 
+            {
+                Dir = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+            }
+
             Position newheadPos = HeadPosition().Translate(Dir);
             GridValue hit = WillHit(newheadPos);    
 
