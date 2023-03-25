be70fd5c293e4f7e7cefb2a03b5ad7bdd2426df0
Mohamad6969 <mohammadsharkawi17@gmail.com>
2023-03-25 16:03:04 +0100

    Unit tests are added

---
+++ SnakeGame/SnakeNUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SnakeNUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SnakeNUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49334bdd-e1d8-4648-ab95-3769500f8545")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SnakeGame/SnakeNUnitTests/SnakeGameTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnakeGame;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SnakeNUnitTests
+{
+    [TestClass]
+    public class SnakeGameTests
+    {
+        [SetUp]
+        private GridValue gridvalue_Empty;
+        private GridValue gridvalue_Snake;
+        private GridValue gridvalue_Outside;
+        public void SetUp()
+        {
+            // ARRANGE
+            gridvalue_Empty = new GridValue();
+
+            // ARRANGE
+            gridvalue_Snake = new GridValue();
+
+            // ARRANGE
+            gridvalue_Outside = new GridValue();
+        }
+        
+          
+
+
+
+        [TestMethod]
+        public void Adding_Food_inGrid()
+        {
+            
+
+
+
+            // act
+
+            if (gridvalue_Empty == GridValue.Empty)
+            {
+                gridvalue_Empty = GridValue.Food;
+            }
+            
+
+            // ASSERT
+            Assert.AreEqual(GridValue.Food, gridvalue_Empty);
+
+        }
+
+        [TestMethod]
+        public void Spawning_Snake()
+        {
+            
+
+
+            // act
+            if(gridvalue_Snake == 0)
+            {
+                gridvalue_Snake = GridValue.Snake;
+            }
+
+            
+
+            // ASSERT
+            Assert.AreEqual(GridValue.Snake, gridvalue_Snake);
+
+        }
+
+        [TestMethod]
+        public void Spawning_OutsideGrid()
+        {
+          
+
+
+            // act
+            if (gridvalue_Outside == 0)
+            {
+                gridvalue_Outside = GridValue.Outside;
+            }
+
+
+
+            // ASSERT
+            Assert.AreEqual(GridValue.Outside, gridvalue_Outside);
+
+        }
+
+       
+
+    }
+}
